98b810f866efd16b7dbc9a6c742b79005bd56d22
Estarossa <65534927+RileyBerry7@users.noreply.github.com>
2025-09-02 16:27:04 -0700

    Added COMMENT_LINE and COMMENT_BLOCK tokens to the grammar and let them be ignored with %ignore.

--- tests/test_1.cpp
+++ tests/test_1.cpp
@@ -1,7 +1,10 @@
-using myType = int;
-[[myAttribute]];
-;
-enum colors;
+// PRIMARY DECLARATIONS
+[[myAttribute]]; // attribute_declaration <- declaration
+;                // empty_declaration <- declaration
+
+// SECONDARY DECLARATION
+using myType = int; // alias_declaration <- block_declaration
+enum colors;        // opaque_enum_declaration <- block_declaration
 
 int * const * main(int ** A::B::C::* const * const volatile * &param1[10][20], char(cast_var), float SCOPE :: * param2)
 {
