283fbd5f46b50436dba0f7cde1d511c89030a6ff
Estarossa <65534927+RileyBerry7@users.noreply.github.com>
2025-09-02 10:43:15 -0700

    Grammar update.

--- tests/test_1.cpp
+++ tests/test_1.cpp
@@ -1,19 +1,12 @@
 ;
+
+enum Colors
+
 int * const * main(int ** A::B::C::* const * const volatile * &param1[10][20], char(cast_var), float SCOPE :: * param2)
 {
     if (1 < 2 + 1 - 1 / 1)
     {
 
-         class myClass
-         {
-
-         };
-
-         enum Color {
-            Red,
-            Green,
-            Blue
-         };
     }
     return 0;
 
