e15e4d346c063d310d72cad5f2c2273a641ccc5a
Estarossa <65534927+RileyBerry7@users.noreply.github.com>
2025-09-05 00:58:21 -0700

    Began reduction of ISO ambiguity.

--- tests/test_1.cpp
+++ tests/test_1.cpp
@@ -1,30 +1,30 @@
-// PRIMARY DECLARATIONS
-template <typename T> ; // template_declaration
-extern template ;  // explicit_instantiation
-template <> ;              // explicit_specialization
-extern "C";                // linkage_specification
-[[myAttribute]];           // attribute_declaration <- declaration
-namespace newNamespace {;} // namespace_definition
-;                          // empty_declaration     <- declaration
-
-// SECONDARY DECLARATION
-asm("mov eax, 1"); // asm_definition
-namespace myNamespace = existingNamespace; // namespace_alias_definition
-using std::cout;                     // using declaration
-using namespace existingNamespace;   // using_directive
-static_assert (1, "Error Message."); // static_assert_declaration
-using myType = int; // alias_declaration       <- block_declaration
-enum colors;        // opaque_enum_declaration <- block_declaration
+//// PRIMARY DECLARATIONS
+//template <typename T> ; // template_declaration
+//extern template ;  // explicit_instantiation
+//template <> ;              // explicit_specialization
+//extern "C";                // linkage_specification
+//[[myAttribute]];           // attribute_declaration <- declaration
+//namespace newNamespace {;} // namespace_definition
+//;                          // empty_declaration     <- declaration
+//
+//// SECONDARY DECLARATION
+//asm("mov eax, 1"); // asm_definition
+//namespace myNamespace = existingNamespace; // namespace_alias_definition
+//using std::cout;                     // using declaration
+//using namespace existingNamespace;   // using_directive
+//static_assert (1, "Error Message."); // static_assert_declaration
+//using myType = int; // alias_declaration       <- block_declaration
+//enum colors;        // opaque_enum_declaration <- block_declaration
 
 int * const * main(int ** A::B::C::* const * const volatile * &param1[10][20], char(cast_var), float SCOPE :: * param2)
 {
-    if (1 < 2 + 1 - 1 / 1)
-    {
-
-    }
+//    if (1 < 2 + 1 - 1 / 1)
+//    {
+//
+//    }
     return 0;
 
-    foo + 1;
-    foo;
+//    foo + 1;
+//    foo;
     1 + 2 * 3 / 4 - 5 % 6;
 }
